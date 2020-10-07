77955c3cd3bb3c4a0e0c088b0def00709b5a12ee
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-07 23:06:28 +0900

    ユーザー新規登録の最低限の機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
+  validates :name, :family_name, :first_name, :family_name_kana, :first_name_kana,  presence: true
+  validates :birthday, presence: true
+end

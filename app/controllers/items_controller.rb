4b367f90f1b85bc78a2b1febcc6d2d9883d3a7f3
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-09 12:32:06 +0900

    自動コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def show

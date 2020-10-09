6cf392322d808fad6cddf03fa9994ff03bb9affc
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-09 14:07:57 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,14 @@ class ItemsController < ApplicationController
     @items = Item.includes(:user).order('created_at DESC')
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
-  #   @category = Category.find(@item.category_id)
-  #   @status   = Status.find(@item.status_id)
-  #   @burden   = DeliveryBurden.find(@item.delivery_burden_id)
-  #   @area     = ShippingArea.find(@item.shipping_area_id)
-  #   @day      = DaysShip.find(@item.days_ship_id)
-  # end
+  def show
+    @item = Item.find(params[:id])
+    @category = Category.find(@item.category_id)
+    @status   = Status.find(@item.status_id)
+    @burden   = DeliveryBurden.find(@item.delivery_burden_id)
+    @area     = ShippingArea.find(@item.shipping_area_id)
+    @day      = DaysShip.find(@item.days_ship_id)
+  end
 
   def new
     @item = Item.new

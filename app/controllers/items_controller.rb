cc951073cabe14236c807d464e13336b5eb16b87
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-09 15:10:30 +0900

    自動コードの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  #後に編集/削除をする際に使用
+  # 後に編集/削除をする際に使用
   # before_action :move_to_show, only: [:edit,:update, :destroy]
 
   def index
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
                                  :price, :image).merge(user_id: current_user.id)
   end
 
-  #後に商品の編集/削除の動作をする際に使用します
+  # 後に商品の編集/削除の動作をする際に使用します
   # def move_to_show
   #   @item = Item.find(params[:id])
   #   unless user_signed_in? && current_user == @item.user

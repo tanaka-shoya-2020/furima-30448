d69c9492d5cd5c30a41ed604e7e88fa7f6cd6191
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-09 14:33:40 +0900

    商品詳細表示機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,19 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  # before_action :move_to_show, only: [:edit,:update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
 
-  # def show
-  #   @item = Item.find(params[:id])
-  #   @category = Category.find(@item.category_id)
-  #   @status   = Status.find(@item.status_id)
-  #   @burden   = DeliveryBurden.find(@item.delivery_burden_id)
-  #   @area     = ShippingArea.find(@item.shipping_area_id)
-  #   @day      = DaysShip.find(@item.days_ship_id)
-  # end
+  def show
+    @item = Item.find(params[:id])
+    @category = Category.find(@item.category_id)
+    @status   = Status.find(@item.status_id)
+    @burden   = DeliveryBurden.find(@item.delivery_burden_id)
+    @area     = ShippingArea.find(@item.shipping_area_id)
+    @day      = DaysShip.find(@item.days_ship_id)
+  end
 
   def new
     @item = Item.new
@@ -34,4 +35,11 @@ class ItemsController < ApplicationController
                                  :delivery_burden_id, :shipping_area_id, :days_ship_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
+
+  # def move_to_show
+  #   @item = Item.find(params[:id])
+  #   unless user_signed_in? && current_user == @item.user
+  #     redirect_to show
+  #   end
+  # end
 end

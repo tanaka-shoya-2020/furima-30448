cbf9284ed5e596a6efa8137bd51eef14d7155b61
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-11 21:08:45 +0900

    購入情報をフォームオブジェクトを用いて別々のテーブルに保管

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,30 @@
 class PurchasesController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+      if @purchase_address.valid?
+        @purchase_address.save
+        redirect_to root_path
+      else
+        render action: :index
+      end
+  end
+
+  private
+
+  def purchase_params
+    #下のコードはpurchase_addressという箱ができていなかったために、引き出すことができず中身を直接引っ張ってくるしかなかった。
+    params.permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
---
+++ app/models/purchase_address.rb
@@ -0,0 +1,22 @@
+class PurchaseAddress
+
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number
+
+  validates :prefecture_id, presence: true
+  validates :municipality,  presence: true
+  validates :address,       presence: true
+  validates :phone_number,  presence: true
+
+  with_options presence: true do
+    validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :phone_number, format: {with: /\A\d{10,11}\z/ }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id )
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end

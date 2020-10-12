a3415d44f5263a1c20734664f6148c61cfc38d09
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-12 15:23:16 +0900

    test

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,7 +13,7 @@ const pay = () => {
       exp_year:  `20${formData.get("exp_year")}`,
       cvc:       formData.get("cvc"),
     };
-
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
       if (status == 200){
         const token = response.id;

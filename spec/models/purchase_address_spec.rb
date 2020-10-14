e895cb32bffdd7075d3e7461455d2c3f413e1023
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-14 11:55:00 +0900

    自動コードの補正

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PurchaseAddress, type: :model do
     sleep 1
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    @purchase_address = FactoryBot.build(:purchase_address,user_id: @user.id, item_id: @item.id)
+    @purchase_address = FactoryBot.build(:purchase_address, user_id: @user.id, item_id: @item.id)
   end
 
   describe '商品購入' do
@@ -54,7 +54,7 @@ RSpec.describe PurchaseAddress, type: :model do
       it '都道府県のidが1に選択されたままの時' do
         @purchase_address.prefecture_id = 1
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@purchase_address.errors.full_messages).to include('Prefecture must be other than 1')
       end
 
       it '市区町村が空の時' do
